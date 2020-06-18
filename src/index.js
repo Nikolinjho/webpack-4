342c581ddd744348ccba74e7520036b4005c72c3
Nikolinjho <nikolinjho@gmail.com>
2020-06-18 14:40:31 +0300

    cleaned up

--- src/index.js
+++ src/index.js
@@ -2,21 +2,4 @@ import "@/styles/styles.css";
 
 
 
-// import xm from "./assets/data.xml"
-// import json from "./assets/json.json";
-// import csv from "./assets/FL_insurance_sample.csv"
 
-import * as $ from "jquery";
-
-import Post from "@models/Post.js";
-
-
-
-// const post = new Post("webpack post title!", banana);
-// $("pre").text(post.toString());
-// console.log("Post to string ", post.toString());  
-
-
-// console.log(csv);
-// console.log("JSON", json)
-// console.log("XML", xm)
