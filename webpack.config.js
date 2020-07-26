816f79b23c9ca91543229d5fce2a9eaffc32bde4
Nikolinjho <nikolinjho@gmail.com>
2020-07-26 17:37:52 +0300

    change paths

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 const environment = (process.env.NODE_ENV || 'development').trim();
 
 if (environment === 'development') {
-    module.exports = require('./webpack.config.development.js');
+    module.exports = require('./config/webpack.config.development.js');
 } else {
-    module.exports = require('./webpack.config.production');
+    module.exports = require('./config/webpack.config.production');
 }
